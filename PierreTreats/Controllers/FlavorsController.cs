43712e0b57d176ccfc0a0f8b0754a113ca23cd89
Benjamin McFarland <benrmcfarland@gmail.com>
2021-01-15 13:14:03 -0800

    Add delete flavor/treat controller methods

--- PierreTreats/Controllers/FlavorsController.cs
+++ PierreTreats/Controllers/FlavorsController.cs
@@ -100,5 +100,14 @@ namespace PierreTreats.Controllers
         _db.SaveChanges();
         return RedirectToAction("Index");
       }
+
+      [HttpPost]
+      public ActionResult DeleteTreat(int joinId)
+      {
+        var joinEntry = _db.TreatFlavor.FirstOrDefault(entry => entry.TreatFlavorId == joinId);
+        _db.TreatFlavor.Remove(joinEntry);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
     }
 }
