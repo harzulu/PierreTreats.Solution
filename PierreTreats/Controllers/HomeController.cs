e18ca3aef8f5f0e8275fed1999455305d42af51a
Benjamin McFarland <benrmcfarland@gmail.com>
2021-01-15 08:42:29 -0800

    Add beginning and boilerplate code

---
+++ PierreTreats/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using PierreTreats.Models;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PierreTreats.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    private readonly PierreTreatsContext _db;
+
+    public HomeController(PierreTreatsContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
